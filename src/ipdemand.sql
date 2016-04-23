9248a68ba2574e9205d925073c3a094d8dd86ca9
ruoyezhiqiu <18380231337@163.com>
2016-04-23 19:23:12 +0800

    update

--- src/ipdemand.sql
+++ src/ipdemand.sql
@@ -1,1 +1,12 @@
-SHOW DATABASES ;
+SHOW DATABASES;
+USE scott;
+DROP TABLE IF EXISTS iptable;
+CREATE TABLE iptable(
+id INT (11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+ip_start VARCHAR (255),
+ip_end VARCHAR (255),
+ip_address VARCHAR(255)
+);
+DESC iptable;
+
+
