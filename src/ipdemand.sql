dcfb42b49e3f82b68949f438acdd1cdaced0cd60
ruoyezhiqiu <18380231337@163.com>
2016-04-24 15:37:41 +0800

    update

--- src/ipdemand.sql
+++ src/ipdemand.sql
@@ -1,12 +1,25 @@
 SHOW DATABASES;
+
 USE scott;
+
+SHOW TABLES;
+
 DROP TABLE IF EXISTS iptable;
-CREATE TABLE iptable(
-id INT (11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-ip_start VARCHAR (255),
-ip_end VARCHAR (255),
-ip_address VARCHAR(255)
+
+CREATE TABLE iptable (
+  id         INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  ip_start   VARCHAR(255),
+  ip_end     VARCHAR(255),
+  ip_address VARCHAR(255)
 );
+
 DESC iptable;
+
+SELECT *
+FROM iptable
+ORDER BY id DESC ;
+
+TRUNCATE TABLE  iptable;
+
 
 
